170713b9dc538924ee4011d8f749b2f21e4699f1
markoveruovič <125287340+user00mv@users.noreply.github.com>
2025-06-06 12:06:12 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelectorAll(".tlacitko");
-
-btn.forEach(function(tlacitko){
-    tlacitko.addEventListener('click', function(){
-        alert("Kliknul jsi na tlačítko");
-    })
-});
